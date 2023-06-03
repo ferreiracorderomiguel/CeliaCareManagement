6ac70300881268538c054da54af42da1bdcec7c8
Miguel Ferreira Cordero <mferreiracordero@gmail.com>
2023-06-03 18:30:21 +0200

    Now News Service and Firebase Service are two individual services

--- src/app/services/firebase-service.ts
+++ src/app/services/firebase-service.ts
@@ -1,1 +1,20 @@
-export class FirebaseService {}
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { News } from '../models/news';
+
+@Injectable()
+export class FirebaseService {
+  constructor(private httpClient: HttpClient) {}
+
+  uploadNews(newsArray: News[]) {
+    this.httpClient
+      .post(
+        'https://celiacare-mfercor326v-default-rtdb.europe-west1.firebasedatabase.app/news.json',
+        newsArray
+      )
+      .subscribe(
+        (response) => console.log('Se ha subido la noticia a Firebase'),
+        (error) => console.log('Error al subir la noticia a Firebase: ' + error)
+      );
+  }
+}
